fc2781804f714269fcfcbd52238e7b6b557340ea
Amey Gupta <amey160202@gmail.com>
2026-01-09 16:40:32 +0530

    minor fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173',"https://bayone-fe.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
